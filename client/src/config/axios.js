f84e14c9f67019b3124b161cf14529bff7a0de8a
ayushsotechy <114767075+ayushsotechy@users.noreply.github.com>
2025-12-30 11:26:36 +0530

    fix axios

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const instance = axios.create({
+export const axiosInstance = axios.create({
     // If the app is built for production, use the current domain (empty string).
     // If running locally, use your hardcoded port 4444.
     baseURL: import.meta.env.MODE === 'production' ? '' : 'http://localhost:5000',
